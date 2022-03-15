4d001219e3c8ac41c15bd646d25e3b9caccc8e3b
driemworks <tonyrriemer@gmail.com>
2022-03-15 17:16:52 -0500

    add tests

--- bin/node-template/pallets/iris-ledger/src/tests.rs
+++ bin/node-template/pallets/iris-ledger/src/tests.rs
@@ -36,5 +36,21 @@ fn iris_ledger_can_lock() {
 
 #[test]
 fn iris_ledger_can_unlock_and_transfer() {
+	let (p, _) = sp_core::sr25519::Pair::generate();
+	let (p2, _) = sp_core::sr25519::Pair::generate();
 
+	let pairs = vec![(p.clone().public(), 10)];
+	new_test_ext_funded(pairs).execute_with(|| {
+		assert_ok!(
+			Origin::signed(p.clone().public()),
+			1,
+		);
+		
+		assert_ok!(
+			Origin::signed(p.clone().public()),
+			p2.clone().public(),
+		);
+		let mut locked_amount = crate::Ledger::<Test>::get(p.public().clone());
+		assert_eq!(0, locked_amount);
+	});
 }
